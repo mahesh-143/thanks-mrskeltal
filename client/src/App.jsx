19fc2e3d1e56776b020e3a882e2eeacc549fc605
mahesh-143 <maheshodedara13@yahoo.com>
2024-03-11 19:49:34 +0530

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,86 @@
+import { useEffect, useState, useRef, createRef } from "react";
+import { socket } from "./socket";
+import video from "./assets/video.mp4";
+
+function getRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function App() {
+  const [thanksList, setThanksList] = useState([]);
+  const [totalThanks, setTotalThanks] = useState();
+
+  const scrollRefs = useRef([]);
+
+  const scrollToElement = () => {
+    if (scrollRefs.current) {
+      scrollRefs.current.scrollTop = scrollRefs.current.scrollHeight;
+    }
+  };
+
+  const sayThanks = () => {
+    const user = JSON.parse(localStorage.getItem("skelUser"));
+    if (user != null) {
+      socket.emit("thanks", { username: user.username, color: user.color });
+    } else {
+      const newUserName = prompt("what's your name hooman?");
+      if (newUserName) {
+        const color = getRandomColor();
+        const newUser = {
+          username: newUserName,
+          color: color,
+        };
+        localStorage.setItem("skelUser", JSON.stringify(newUser));
+        socket.emit("thanks", { username: newUserName, color });
+      }
+    }
+  };
+
+  const onTotalThanks = (value) => {
+    setTotalThanks(value);
+  };
+
+  useEffect(() => {
+    socket.on("connect");
+    socket.on("newUser", (newUser) => {
+      setThanksList((prevThanks) => [...prevThanks, newUser]);
+    });
+
+    socket.on("totalThanks", onTotalThanks);
+
+    scrollToElement();
+
+    return () => {
+      socket.off("newUser");
+    };
+  }, [thanksList]);
+
+  return (
+    <main className="wrapper">
+      <h1 className="title">Thank Mr Skeltal</h1>
+      <video width="750" height="500" loop autoPlay muted className="video">
+        <source src={video} type="video/mp4" />
+      </video>
+
+      <div className="count">Total Thanks {totalThanks}</div>
+      <ul className="chat" ref={scrollRefs}>
+        {thanksList.map((userData, index) => (
+          <li key={index} className="messgae">
+            <span style={{ color: userData.color }} className="username">
+              {userData.username}
+            </span>{" "}
+            thanked Mr Skeltal
+          </li>
+        ))}
+      </ul>
+      <button onClick={sayThanks}>Thank Mr Skeltal</button>
+    </main>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,71 @@
+import express from "express";
+import { createServer } from "node:http";
+import { Server } from "socket.io";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const server = createServer(app);
+
+app.use(cors());
+
+const DATABASE_URL = process.env.DATABASE_URL;
+try {
+  mongoose.connect(DATABASE_URL);
+} catch (err) {
+  console.log(err);
+}
+const TotalThanks = mongoose.model("TotalThanks", { count: Number });
+
+const io = new Server({
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
+
+io.on("connection", async (socket) => {
+  console.log("a user connected", socket.id);
+
+  const totalThanks = await getTotalThanks();
+  socket.emit("totalThanks", totalThanks);
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+
+  socket.on("thanks", async (userData) => {
+    const updatedCount = await incrementTotalThanks();
+    io.emit("newUser", userData, totalThanks);
+    io.emit("totalThanks", updatedCount);
+  });
+});
+
+async function getTotalThanks() {
+  try {
+    const result = await TotalThanks.findOne();
+    return result ? result.count : 0;
+  } catch (error) {
+    console.error("Error reading total thanks from database:", error);
+    return 0;
+  }
+}
+
+async function incrementTotalThanks() {
+  try {
+    const updatedCount = await TotalThanks.findOneAndUpdate(
+      {},
+      { $inc: { count: 1 } },
+      { new: true, upsert: true },
+    );
+    return updatedCount.count;
+  } catch (error) {
+    console.error("Error updating total usernames in database:", error);
+    return 0;
+  }
+}
+
+io.listen(3000, () => {
+  console.log("server running at http://localhost:3000");
+});
