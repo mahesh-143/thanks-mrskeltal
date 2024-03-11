c95e482d2db07d87e69e032370052c48bcdbaff2
mahesh-143 <maheshodedara13@yahoo.com>
2024-03-11 20:49:48 +0530

    add production url

--- client/src/socket.js
+++ client/src/socket.js
@@ -2,6 +2,8 @@ import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
 const URL =
-  process.env.NODE_ENV === "production" ? undefined : "http://localhost:3000";
+  process.env.NODE_ENV === "production"
+    ? "https://thanks-mrskeltal-1.onrender.com"
+    : "http://localhost:3000";
 
 export const socket = io(URL);
